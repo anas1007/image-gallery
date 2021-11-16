29cf80bf042c5fe6c37d69f2df13c0d31ed34503
Muhammad Anas - External <muhammadanas.israr@systemsltd.com>
2021-11-16 14:10:19 +0500

    First Commit

---
+++ app/Models/Album.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Console\Concerns\InteractsWithIO;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Album extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    protected $fillable = ['title'];
+}
